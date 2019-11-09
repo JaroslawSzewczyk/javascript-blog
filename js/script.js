dd1b71b4e11065cc2033c0ba160d43dc893d3063
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2019-11-09 12:10:18 +0000

    Add class active to article

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
-    console.log(event);
 
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -12,7 +11,6 @@ const titleClickHandler = function (event) {
 
     /* [DONE] add class 'active' to the clicked link */
 
-    console.log('clicked', clickedElement);
     clickedElement.classList.add('active');
 
     /* [DONE] remove class 'active' from all articles */
@@ -26,14 +24,14 @@ const titleClickHandler = function (event) {
     /* [DONE] get 'href' attribute from the clicked link */
 
     const hrefAttribute = clickedElement.getAttribute('href');
-    console.log(hrefAttribute);
-
 
     /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(hrefAttribute);
 
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
